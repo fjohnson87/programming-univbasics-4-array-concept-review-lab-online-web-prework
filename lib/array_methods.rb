c021c63de3a385ef074c7d8c36983d7c75b14635
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-05 00:02:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,19 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  while
+  scale = [2,3,4,5]
+  find_element_index (scale)
+  value_to_find (scale)
 end
 
 def find_max_value(array)
   # Add your solution here
+  hill = [1,2,3,4,5,4,3,2,1]
+  find_max_value(hill)
 end
 
 def find_min_value(array)
   # Add your solution here
+  zig_zag = [500,4,1000,5,250]
+  find_min_value(zig_zag)
 end
